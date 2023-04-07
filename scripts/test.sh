8fe91e2a479ec1b1982875c043f6faf01affe50e
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-04-06 21:28:04 -0600

    double-init patch

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,3 +2,4 @@
 py src/__init__.py test/in/test.csv -nospace -id Name -o test/out/csv.luau
 py src/__init__.py test/in/test.xlsx -nospace -id Name -o test/out/xlsx.luau
 py src/__init__.py -sheet 1-oZ16B2k9k-jj3APokzYILdPj1oavLxmv3CMUQ9Zkr0 -nospace -o test/out/google.luau -id Id
+
